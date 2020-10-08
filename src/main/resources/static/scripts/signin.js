e61ccd2d202f41819e62ca9e6d4aadccae1aeb9d
AveryDeSean <62083127+AveryDeSean@users.noreply.github.com>
2020-10-08 11:41:35 -0500

    Modified javascript code and added it to signin.html

--- src/main/resources/static/scripts/signin.js
+++ src/main/resources/static/scripts/signin.js
@@ -1,12 +1,42 @@
-function ValidationEvent(){
-    var employee = document.getElementById("employeeid").value;
-    var password = document.getElementById("pword").value;
-
-    if(employee < 0){
-        alert("Employee ID must be greater than 0");
+    document.addEventListener("DOMContentLoaded", function(event) {
+        const employeeIdElement = getEmployeeIdEditElement();
+        employeeIdElement.focus();
+        employeeIdElement.select();
+    });
+    
+    function validateForm() {
+        const employeeIdElement = getEmployeeIdEditElement();
+        if (isNaN(Number(employeeIdElement.value))
+            || (Number(employeeIdElement.value) <= 0)) {
+    
+            displayError("Please provide a valid employee ID.");
+    
+           // employeeIdElement.focus();
+           // employeeIdElement.select();
+            
+            return false;
+        }
+    
+        const passwordEditElement = getPasswordEditElement();
+        if ((passwordEditElement.value == null)
+            || (passwordEditElement.value.trim() === "")) {
+    
+            displayError("Your password is blank, please provide a real password");
+    
+           // passwordEditElement.focus();
+          //  passwordEditElement.select();
+            
+            return false;
+        }
+    
+        return true;
     }
-    else if(password == ''){
-        alert("Password field is blank");
+    
+    //Getters and setters
+    function getPasswordEditElement() {
+        return document.getElementById("pword");
     }
-
-}
+    
+    function getEmployeeIdEditElement() {
+        return document.getElementById("employeeid");
+    }
