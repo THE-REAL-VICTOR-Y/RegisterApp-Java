ad67c1f6e60b29ff02362e6837ee85b4e506fcb6
kaylaboyd430 <70405463+kaylaboyd430@users.noreply.github.com>
2020-10-11 18:41:28 -0500

    sign in route

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,5 +1,7 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
@@ -8,15 +10,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.controllers.enums.QueryParameterNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
 
 @Controller
 @RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
 	// TODO: Route for initial page load
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView viewSignIn(@RequestParam Map< String, String> queryParameters)
+	{
+		
+	try 
+	{
+		this.activeEmployeeExistsQuery.execute();
+	} 
+	catch (Exception e) 
+	{
+		//TODO: handle exception
+		return new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.EMPLOYEE_DETAIL.getRoute()));
+	}
+
+	ModelAndView mav = this.setErrorMessageFromQueryString(new ModelAndView(ViewNames.SIGN_IN.getViewName()), queryParameters);
+
+	if(queryParameters.containsKey(QueryParameterNames.EMPLOYEE_ID.getValue()))
+	{
+		mav.addObject(ViewModelNames.EMPLOYEE_ID.getValue(), queryParameters.get(QueryParameterNames.EMPLOYEE_ID.getValue()));
+	}
+
+		return mav;
+	}
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ModelAndView performSignIn(
+	public ModelAndView performSignIn( EmployeeSignIn eSignIn,
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
 		HttpServletRequest request
 	) {
@@ -24,9 +53,24 @@ public class SignInRouteController extends BaseRouteController {
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
+		try 
+		{
+			this.employeeSignInCommand.setSessionId(request.getSession().getId())
+				.setEmployeeSignIn(employeeSignIn).execute();
+		}
+		 catch (Exception e) 
+		{
+			ModelAndView mav = new ModelAndView(ViewNames.SIGN_IN.getViewName());
 
-		return new ModelAndView(
-			REDIRECT_PREPEND.concat(
-				ViewNames.MAIN_MENU.getRoute()));
+			mav.addObject (ViewModelNames.ERROR_MESSAGE.getValue(), e.getMessage());
+			mav.addObject (ViewModelNames.EMPLOYEE_ID.getValue(), eSignIn.getEmployeeId());
+			return mav;
+		}
+
+		return new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.MAIN_MENU.getRoute()));
 	}
+
+	@Autowired private EmployeeSignIn employeeSignInCommand;
+	@Autowired private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
+
 }
