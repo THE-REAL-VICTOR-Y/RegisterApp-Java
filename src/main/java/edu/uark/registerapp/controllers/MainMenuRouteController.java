fd7476cfae61357a4a3fe49425b321830ef50005
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2020-10-11 23:21:34 -0500

    Update MainMenuRouteController.java

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.ui.ModelMap;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,10 +47,11 @@ public class MainMenuRouteController extends BaseRouteController {
 		
 		return modelAndView;
 	}
-	// @GetMapping("/redirectWithRedirectPrefix")
-    // public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
-    //     model.addAttribute("attribute", "redirectWithRedirectPrefix");
-    //     return new ModelAndView("redirect:/https://victor-y-app.herokuapp.com/signin", model);
-    // }
+	@GetMapping
+	("/redirectWithRedirectPrefix")
+    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
+      model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        return new ModelAndView("redirect:/https://victor-y-app.herokuapp.com/signin", model);
+     }
 }
 
